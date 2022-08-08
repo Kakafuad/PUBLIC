28e878d1a5ce42e37aad87273d707733745bbff5
FAHAD HOSSAIN REFAT <109792845+REFAT-156@users.noreply.github.com>
2022-08-08 11:20:27 +0600

    Update Okay.py

--- Okay.py
+++ Okay.py
@@ -1,23 +1,18 @@
-import os, platform
-
-try:
-
-    import requests
-
-except:
-
-    os.system('pip2 install requests')
-
-import requests
-
-bit = platform.architecture()[0]
-
-if bit == '64bit':
-
-    from public62 import menu
-
-    menu()
-
-elif bit == '32bit':
-
-    print "Opps Your Device Not Supported"
+import os, platform, time
+print('\n\x1b[1;37m[•] Checking Update...');time.sleep(0.5)
+os.system('git pull')
+def Run():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            print("\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
+            print('[•] Follow My Github First')
+            os.system('xdg-open https://github.com/REFAT-156')
+            import public62
+        elif bit == '32bit':
+            print("\n\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
+            print('[•] Follow My Github First')
+            os.system('xdg-open https://github.com/REFAT-156')
+            import public32
+        else:
+            exit('\033[1;31m[×] Connection Error')
+Run()
